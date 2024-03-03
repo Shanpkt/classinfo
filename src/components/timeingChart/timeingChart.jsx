34656b1858b47edb832df13b13242c9396cdf741
Shantanu <shantanupathak01@gmail.com>
2024-03-04 01:40:12 +0530

    fourth commit

--- src/components/timeingChart/timeingChart.jsx
+++ src/components/timeingChart/timeingChart.jsx
@@ -4,7 +4,7 @@ function TimeingChart() {
   return (
     <div className='time_chart'>
         <h4>Timeings</h4>
-        <p>8AM TO 10AM</p>
+        <p>9PM TO 11PM</p>
         <h4>Weekdays</h4>
         <p>Monday - Friday</p>
     </div>
